16a5a09aaf6bcadef5319de7dee444ea17a9c282
Aleksei Chashin <jabuj@yandex-team.ru>
2021-04-22 20:19:41 +0300

    Added navigation, tailwind config and simple layout

---
+++ client/src/components/global/navigation/navigation.tsx
@@ -0,0 +1,28 @@
+import {Link} from 'react-router-dom'
+import styles from './navigation.module.scss'
+
+export const NAVIGATION_PADDING = 'pt-16'
+const NAVIGATION_ITEMS: {label: string; target: string}[] = [
+    {label: 'Каталог товаров', target: '/catalog'},
+    {label: 'О фонде', target: '/about'},
+    {label: 'Контакты', target: '/contacts'},
+    {label: 'Личный кабинет', target: '/profile'},
+]
+
+export const Navigation = () => (
+    <nav className={'fixed inset-x-0 top-0 h-16 bg-gray-400'}>
+        <div className={'container flex h-full'}>
+            {NAVIGATION_ITEMS.map(({label, target}) => (
+                <Link
+                    className={
+                        'flex-1 h-full flex justify-center items-center ' +
+                        'hover:bg-hover active:bg-active duration-75'
+                    }
+                    to={target}
+                >
+                    {label}
+                </Link>
+            ))}
+        </div>
+    </nav>
+)
--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,7 +1,20 @@
-import {Router} from 'src/pages/Router'
+import {
+    Navigation,
+    NAVIGATION_PADDING,
+} from 'src/components/global/navigation/navigation'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Index} from 'src/pages/Index'
+import clsx from 'clsx'
 
 export const App = () => (
-    <div>
-        <Router />
+    <div className={clsx(NAVIGATION_PADDING, 'bg-gray-100 min-h-screen')}>
+        <BrowserRouter>
+            <Navigation />
+            <div className={'container bg-gray-200 min-h-screen'}>
+                <Switch>
+                    <Route exact path="/" component={Index} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     </div>
 )
