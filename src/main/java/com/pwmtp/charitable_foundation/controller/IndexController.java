847710009b105d85b3d675e20e4720145461d7f6
Mima <56038155+dr9ken@users.noreply.github.com>
2021-04-22 18:04:03 +0300

    html files is back

--- src/main/java/com/pwmtp/charitable_foundation/controller/IndexController.java
+++ src/main/java/com/pwmtp/charitable_foundation/controller/IndexController.java
@@ -1,13 +1,12 @@
 package com.pwmtp.charitable_foundation.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class IndexController {
     @GetMapping
     public Object getPage() {
-        return new ModelAndView("index");
+        return "index";
     }
 }
