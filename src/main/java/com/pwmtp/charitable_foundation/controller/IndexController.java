af52b353df032f9ae9ebc1ec5ab72d5de06759ef
Mima <56038155+dr9ken@users.noreply.github.com>
2021-04-23 00:45:05 +0300

    IndexControllerFix

--- src/main/java/com/pwmtp/charitable_foundation/controller/IndexController.java
+++ src/main/java/com/pwmtp/charitable_foundation/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/")
 public class IndexController {
     @GetMapping
     public Object getPage() {
