67d3e5f00374b6f531d5bdc34b25a2dbd6117ae2
Mima <56038155+dr9ken@users.noreply.github.com>
2021-04-24 17:12:26 +0300

    Added ResponseEntity to UserController.getUser if user!=null

--- src/main/java/com/pwmtp/charitable_foundation/controller/UserController.java
+++ src/main/java/com/pwmtp/charitable_foundation/controller/UserController.java
@@ -6,16 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
     @RequestMapping(value = "/me", method = RequestMethod.POST)
-    public @ResponseBody Object getUser(@AuthenticationPrincipal User user) {
+    public ResponseEntity<Object> getUser(@AuthenticationPrincipal User user) {
         return user != null
-                ? user
+                ? new ResponseEntity<>(user, HttpStatus.OK)
                 : new ResponseEntity<>("user is not logged in", HttpStatus.BAD_REQUEST);
     }
 }
