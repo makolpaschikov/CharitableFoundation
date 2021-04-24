758c2cb7e31615fcd2b8e2466b6bbf04bac99b77
Mima <56038155+dr9ken@users.noreply.github.com>
2021-04-24 20:59:29 +0300

    Added services to ProductController.java

--- src/main/java/com/pwmtp/charitable_foundation/controller/ProductController.java
+++ src/main/java/com/pwmtp/charitable_foundation/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.pwmtp.charitable_foundation.controller;
 
+import com.pwmtp.charitable_foundation.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+    private final ProductService USER_SERVICE;
+    private final ProductService PRODUCT_SERVICE;
+
+    @Autowired
+    public ProductController(ProductService USER_SERVICE, ProductService PRODUCT_SERVICE) {
+        this.USER_SERVICE = USER_SERVICE;
+        this.PRODUCT_SERVICE = PRODUCT_SERVICE;
+    }
+
     @RequestMapping(value = "add-product", method = RequestMethod.PUT)
     public void addProduct() {
         // TODO: not yet implemented
