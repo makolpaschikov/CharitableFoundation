884cef4b905ce69702574ddcc8756726c0f138a5
Aleksei Chashin <jabuj@yandex-team.ru>
2021-04-25 21:34:51 +0300

    Added contact fetching

--- client/src/pages/Product/Product.tsx
+++ client/src/pages/Product/Product.tsx
@@ -1,12 +1,15 @@
-import {FC, useEffect} from 'react'
+import ky from 'ky'
+import {FC, useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {RouteComponentProps} from 'react-router'
 import {RetryLoading} from 'src/components/shared/RetryLoading'
 import {Spinner} from 'src/components/shared/Spinner'
 import {AppDispatch, AppState} from 'src/store'
 import {loadProducts, Product as TProduct} from 'src/store/products'
+import {ENDPOINTS} from 'src/util/api'
 import {Category} from 'src/util/categories'
 import {LoadingStatus} from 'src/util/loading-status'
+import {Contact} from 'src/pages/Contacts'
 
 export const Product = ({
     match: {
@@ -71,15 +74,70 @@ const View: FC<{product: TProduct}> = ({product}) => {
                 </div>
                 <h1 className={'text-3xl font-bold mb-4'}>{product.name}</h1>
                 <p className={'mb-6'}>{product.description}</p>
-                <button
-                    className={
-                        'bg-primary-light py-4 px-8 text-white rounded-lg'
-                    }
-                    type={'submit'}
-                >
-                    Связаться
-                </button>
+                <GetContact product={product} />
             </div>
         </div>
+    )
+}
+
+const GetContact: FC<{product: TProduct}> = ({product}) => {
+    const [status, setStatus] = useState(LoadingStatus.NONE)
+    const [data, setData] = useState<null | {email: string; name: string}>(null)
+
+    const fetch = async () => {
+        if (status === LoadingStatus.SUCCESS) return
+        setStatus(
+            status === LoadingStatus.ERROR
+                ? LoadingStatus.RETRYING
+                : LoadingStatus.PENDING
+        )
+        try {
+            const result = await ky
+                .get(ENDPOINTS.getContacts + `?id=${product.id}`, {
+                    credentials: 'include',
+                })
+                .json<{email: string; name: string}>()
+            setData(result)
+        } catch (e) {
+            setStatus(LoadingStatus.ERROR)
+        }
+    }
+    const getView = () => {
+        switch (status) {
+            case LoadingStatus.NONE:
+                return null
+            case LoadingStatus.PENDING:
+                return <Spinner />
+            case LoadingStatus.ERROR:
+            case LoadingStatus.RETRYING:
+                return (
+                    <RetryLoading
+                        message={'Не удалось загрузить контакты'}
+                        retrying={status === LoadingStatus.RETRYING}
+                        retry={fetch}
+                    />
+                )
+            case LoadingStatus.SUCCESS:
+                return (
+                    <>
+                        <Contact label={'Название'} value={data!.name} />
+                        <Contact
+                            label={'Электронная почта'}
+                            value={data!.email}
+                        />
+                    </>
+                )
+        }
+    }
+    return (
+        <>
+            <button
+                className={'bg-primary-light py-4 px-8 text-white rounded-lg'}
+                onClick={fetch}
+            >
+                Связаться
+            </button>
+            {getView()}
+        </>
     )
 }
